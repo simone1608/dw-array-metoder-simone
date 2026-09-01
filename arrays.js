6013a073561928e49e97cff48b97a11364930525
Simone <simonesnehvide@gmail.com>
2026-09-01 09:23:42 +0200

    Har lavet opgave 1 og 2 i array metoder opgaven

--- arrays.js
+++ arrays.js
@@ -1,9 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
+
+    // Opgave 1
+    let newNumbers = numbersArray.filter(function (number) {
+        return number > 20;
+    });
+
+    console.log(newNumbers);
+
+    // Opgave 2
+    catsArray.push("sniffles")
+    console.log(catsArray);
 
 }) // ends DOMContentLoaded
