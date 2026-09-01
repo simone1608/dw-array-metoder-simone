c85df4203ec746c520a0bdf64b0c9ae14b7d56ad
Simone <simonesnehvide@gmail.com>
2026-09-01 09:31:43 +0200

    Har skrevet kommentare på mine opgaver og lavet opgave 3, a og b

--- arrays.js
+++ arrays.js
@@ -12,9 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     console.log(newNumbers);
+    //Jeg bruger filter da den kalder alle elementerne tilbage og tjekker for vilke er sande og sender kun de sande ud igen. 
 
     // Opgave 2
     catsArray.push("sniffles")
     console.log(catsArray);
+    //Jeg bruger push tager det nye element og tilføjer det til slutningen af arrayet og sender det nye array tilbage med den nye element.
 
+    //opgave 3 a
+    let hasTrixie = dogsArray.includes("Trixie");
+    console.log(hasTrixie);
+
+    //opgave 3 b
+    let hasBaxter = dogsArray.includes("Baxter");
+    console.log(hasBaxter);
+    //Jeg bruger includes da det tjekker arrayet for om der er et specifikt element og derefter udskriver true eller false derudfra
 }) // ends DOMContentLoaded
